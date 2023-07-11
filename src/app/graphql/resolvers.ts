74646b9b9ac6d7d6498a611c417eec7acab6a34d
Alex Tarasiuk <antar37@gmail.com>
2023-07-11 15:55:20 -0600

    Added where and ordering to graph Co-authored-by: Nathan Tarasiuk <Ntarasiuk@gmail.com>

--- src/app/graphql/resolvers.ts
+++ src/app/graphql/resolvers.ts
@@ -1,70 +1,93 @@
 import { GraphQLTypeResolver } from "graphql";
 
-const getRelatedIds = (type, item) => item?.relationships?.[type]?.data?.length ? item?.relationships?.[type]?.data?.map((f) => f.id) : [item?.relationships?.[type]?.data];
-const getRelatedData = (data = [], Ids = []) => data.included.filter((f) => Ids.includes(f.id)) || null
-const getRelatedFieldNames = (info, name) => info?.fieldNodes[0].selectionSet?.selections?.map((f) => f.name.value === name)
+const getRelatedIds = (type, item) =>
+  item?.relationships?.[type]?.data?.length
+    ? item?.relationships?.[type]?.data?.map((f) => f.id)
+    : [item?.relationships?.[type]?.data];
+const getRelatedData = (data = [], Ids = []) =>
+  data.included.filter((f) => Ids.includes(f.id)) || null;
+const getRelatedFieldNames = (info, name) =>
+  info?.fieldNodes[0].selectionSet?.selections?.map(
+    (f) => f.name.value === name
+  );
 
-const person_includes = ["addresses",
-"field_data",
-"emails",
-"households",
-"marital_status",
-"inactive_reason",
-"name_prefix",
-"name_suffix",
-"person_apps",
-"organization",
-"phone_numbers",
-"platform_notifications",
-"primary_campus",
-"school"]
-
+const person_includes = [
+    "addresses",
+    "emails",
+    "field_data",
+    "households",
+    "inactive_reason",
+    "marital_status",
+    "name_prefix",
+    "name_suffix",
+    "organization",
+    "person_apps",
+    "phone_numbers",
+    "platform_notifications",
+    "primary_campus",
+    "school",
+    "social_profiles"
+];
 
 // find out which elements came in through GraphQL, and create an array of includes options
-const getIncludes = (info, includesArray = []) => includesArray.map(name => 
-  ( {
-     name, 
-     data: getRelatedFieldNames(info, name) 
-   })
-   ).filter(f => f.data)
-   .map(f => f.name).join(',')
+const getIncludes = (info, includesArray = []) =>
+  includesArray
+    .map((name) => ({
+      name,
+      data: getRelatedFieldNames(info, name),
+    }))
+    .filter((f) => f.data)
+    .map((f) => f.name)
+    .join(",");
 
-   const zipRelatedData = (data, item, includesArray ) => {
-    const relatedData = {}
-    includesArray.map((type) => {
-      console.log(
-        item?.relationships?.[type]?.data
-        );
-      const ids = getRelatedIds(type, item)
-      relatedData[type] = getRelatedData(data, ids)
-    })
-    return relatedData
-   }
+const zipRelatedData = (data, item, includesArray) => {
+  const relatedData = {};
+  includesArray.map((type) => {
+    const ids = getRelatedIds(type, item);
+    relatedData[type] = getRelatedData(data, ids);
+  });
+  return relatedData;
+};
+const formatWhere = (where) =>
+  where
+    ? Object.entries(where)
+        .map(([key, value]) => `&where[${key}]=${value}`)
+        .join("")
+    : "";
+
 const resolvers = {
-    Query: {
-      people(parent, params, context, info) {
-        const limit = params.limit || 10
+  Query: {
+    people(parent, params, context, info) {
+      const { limit = 10, where, order } = params;
+      const whereArg = where ? formatWhere(where) : '&where[status]=active'
+      const orderArg = `&order=${order.sort === 'desc' ? '-' : ''}${order.field}`
+      const includes = getIncludes(info, person_includes);
+      const getUsers = async () => {
+        let response = await fetch(
+          `https://api.planningcenteronline.com/people/v2/people?per_page=${limit}&include=${includes}${whereArg}${orderArg}`,
+          {
+            method: "GET",
+            headers: {
+              Authorization:
+                "Basic " +
+                btoa(process.env.PCO_APP_ID + ":" + process.env.PCO_SECRET),
+            },
+          }
+        );
+        let data = await response.json();
+        let result = data.data.map((e) => {
+          const relatedData = zipRelatedData(data, e, person_includes);
+          return {
+            ...e,
+            ...relatedData,
+          };
+        });
 
-        const includes = getIncludes(info, person_includes)
-        const getUsers = async () => {
-          let response = await fetch(`https://api.planningcenteronline.com/people/v2/people?per_page=${limit}&include=${includes}`, {method:'GET', 
-          headers: {'Authorization': 'Basic ' + btoa(process.env.PCO_APP_ID+":"+process.env.PCO_SECRET)}});
-          let data = await response.json();
-          let result = data.data.map((e) => {
-            const relatedData = zipRelatedData(data, e, person_includes)
-            console.log(relatedData)
-            return {
-              ...e,
-              name: `${e.attributes.first_name} ${e.attributes.last_name}`,
-             ...relatedData,
-            };
-          });
-         
-          return result;
-        }
-        return getUsers();
-      },
+        return result;
+      };
+      return getUsers();
     },
-  }
+  },
+};
 
-  export default resolvers
+export default resolvers;
--- src/app/graphql/typeDefs.ts
+++ src/app/graphql/typeDefs.ts
@@ -2,15 +2,157 @@ import gql from "graphql-tag"
 
 const typeDefs = gql`
 type Query {
-  people(limit: Int): [Person!]!
+  people(
+    limit: Int, 
+    where: PersonWhereAttributes,
+    order: OrderInput
+  ): [Person!]!
 }
 
+input OrderInput {
+  """ this will append a (-) to the field name if desc """
+  sort: sortEnum = asc
+  field: PersonOrderEnum 
+}
+
+enum sortEnum {
+  asc
+  desc
+}
+
+
 type Person {
+  type: String
+  id: String
+  attributes: PersonAttributes
+  links: PersonLinks
+}
+
+enum PersonOrderEnum {
+  avatar_url
+  name
+  child
+  created_at
+  event_permissions_type
+  first_name
+  gender
+  given_name
+  last_name
+  middle_name
+  name_prefix
+  name_suffix
+  pending_request_count
+  people_permissions_type
+  permissions
+  resolves_conflicts
+  resources_permissions_type
+  room_permissions_type
+  site_administrator
+  status
+  updated_at
+}
+
+input PersonWhereAttributes {
+  given_name: String
+  first_name: String
+  nickname: String
+  middle_name: String
+  last_name: String
+  birthdate: String
+  anniversary: String
+  gender: String
+  grade: String
+  child: String
+  graduation_year: String
+  site_administrator: String
+  accounting_administrator: String
+  people_permissions: String
+  membership: String
+  inactivated_at: String
+  status: String
+  medical_notes: String
+  created_at: String
+  updated_at: String
+  search_name: String
+  search_phone_number: String
+  search_phone_number_e164: String
+  search_name_or_email: String
+  search_name_or_email_or_phone_number: String
+  remote_id: String
+  id: String
+}
+
+enum StatusEnum {
+  inactive
+  active
+}
+
+type PersonAttributes {
+  avatar_url: String
   name: String
+  child: Boolean
+  contact_data: PersonContactData
+  created_at: String
+  event_permissions_type: String
+  given_name: String
+  first_name: String
+  gender: String
+  has_access: Boolean
+  last_name: String
+  middle_name: String
+  name_prefix: String
+  name_suffix: String
+  pending_request_count: Int
+  people_permissions_type: String
+  permissions: Int
+  resolves_conflicts: Boolean
+  resources_permissions_type: String
+  room_permissions_type: String
+  site_administrator: Boolean
+  status: String
+  updated_at: String
   emails: [Email]
   addresses: [Address]
   households: [Household]
 }
+
+type PersonContactData {
+  email_addresses: [EmailAddress]
+  addresses: [Address]
+  phone_numbers: [PhoneNumber]
+}
+
+type EmailAddress {
+  address: String
+  location: String
+  primary: Boolean
+  blocked: Boolean
+}
+
+type Address {
+  street: String
+  street_line_1: String
+  street_line_2: String
+  city: String
+  state: String
+  zip: String
+  location: String
+  primary: Boolean
+}
+
+type PhoneNumber {
+  number: String
+  carrier: String
+  location: String
+  primary: Boolean
+}
+
+type PersonLinks {
+  self: String
+  html: String
+}
+
+
 
 type Email {
   type: String
